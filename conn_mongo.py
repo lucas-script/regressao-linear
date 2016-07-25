edb9c783814256252f5bb44c04acdd4c0236448a
Lucas Machado <lucas.engcom@gmail.com>
2016-07-25 18:54:34 -0300

    fodase

--- conn_mongo.py
+++ conn_mongo.py
@@ -1,8 +1,17 @@
 from pymongo import MongoClient
-import numpy
+import numpy as np
 
 client = MongoClient("mongodb://localhost:27017")
-db = client['quali']
-cursor = db.turmas.find()
+db = client['test']
+cursor = db.inflacao.find()
 for document in cursor:
-	print(document['_id'])
+	print(document)
+
+
+def mmq(x, y):
+    x = np.insert(x, 0, 1, axis=1)
+    x_t = np.transpose(x)
+    xt_x = np.dot(x_t, x)
+    inverse_xt_x = np.linalg.inv(xt_x)
+    xt_y = np.dot(x_t, y)
+    return np.dot(inverse_xt_x, xt_y)
